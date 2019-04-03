02d0d134b66ec9f077ebb0d9e270f7cd8730e8e6
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2019-04-03 21:27:39 +0300

    parens init

---
+++ gen/parens.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import numpy as np
+from typing import Tuple
+
+
+def parens(n: int) -> Tuple[np.array, np.array]:
+	"""
+	:param n: half of nums to div into lists
+	:return: two lists with permutation of [0, 2n)
+	"""
+	p = np.random.permutation(np.arange(n << 1))
+	return p[:n], p[n:]
+
+
+if __name__ == '__main__':
+	n = 7
+
+	m_list, f_list = parens(n)
+
+	print(m_list, f_list, sep='\n')
