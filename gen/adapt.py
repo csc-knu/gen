19cfb4f812504e4b00f512625890f6a5a5aa826a
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2019-05-10 02:22:48 +0300

    gen adapt periodic to p docs and example

--- gen/adapt.py
+++ gen/adapt.py
@@ -3,37 +3,37 @@ import numpy as np
 from itertools import accumulate 
 
 
-def __adapt(periodic: np.array, n: int) -> np.array:
+def __adapt(p: np.array, n: int) -> np.array:
 	"""
-	:param periodic: n nums
-	:param n: len periodic
+	:param p: n nums
+	:param n: len p
 	:return: adaptability
 	"""
-	periodic -= np.min(periodic)
+	p -= np.min(p)
 
-	periodic /= np.sum(periodic)
+	p /= np.sum(p)
 
 	num = np.zeros(n)
 
-	for roll in np.random.choice(np.arange(n), n, p=periodic):
+	for roll in np.random.choice(np.arange(n), n, p=p):
 		num[roll] += 1
 
 	return num.astype(int)
 
 
-def adapt(periodic: np.array) -> np.array:
+def adapt(p: np.array) -> np.array:
 	"""
-	:param periodic: n nums
+	:param p: n nums
 	:return: adaptability
 	"""
-	n: int = len(periodic)
+	n: int = len(p)
 
-	return __adapt(periodic, n)
+	return __adapt(p, n)
 
 
 if __name__ == '__main__':
 	n = 4
 
-	periodic = np.array([1.1, 1.3, 1.7, 1.9])
+	p = np.array([1.1, 1.3, 1.7, 1.9])
 
-	print(adapt(periodic))
+	print(adapt(p))
