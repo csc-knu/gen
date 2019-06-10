85dcda200712e01628bd7fb58dbe1684b5de68b8
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2019-06-10 14:41:20 +0300

    Updates from Overleaf

--- gen/code/main.py
+++ gen/code/main.py
@@ -13,29 +13,13 @@ from best_worst import best, worst
 
 
 np.set_printoptions(linewidth=100)
-
 np.random.seed(65537)
 
 n, _m = 1 << 10, 20
-
 x_min, x_max = np.repeat(-2, _m), np.repeat(2, _m)
-
-# x_min, x_max = np.repeat(-5.12, _m), np.repeat(5.12, _m)
-
 g_dec = generation_dec(n, x_min, x_max)
-
-# print(f'g_dec = {g_dec}')
-
 eps = 1e-5
-
 nn, dd, NN = bin_dec_param(x_min, x_max, eps)
-
-# print(
-# 	f'nn = {nn}\n'
-# 	f'dd = {dd}\n'
-# 	f'NN = {NN}'
-# )
-
 p = 2e-3
 
 def rastrigin(x: np.array) -> float:
@@ -44,64 +28,18 @@ def rastrigin(x: np.array) -> float:
 
 for _ in range(1, 1 << 10):
 	g_bin = a_cod_binary(g_dec, x_min, nn, dd)
-
-	# print(f'g_bin = {g_bin}')
-
 	g_bin, mutation_count = mutation(g_bin, p)
-
-	# print(
-	# 	f'g_bin = {g_bin}\n'
-	# 	f'mutation_count = {mutation_count}'
-	# )
-
 	m, f = parents(n >> 1)
-
-	# print(
-	# 	f'm = {m}\n'
-	# 	f'f = {f}'
-	# )
-
 	g_bin = crossover(g_bin, m, f)
-
-	# print(f'g_bin = {g_bin}')
-
 	g_dec = a_cod_decimal(g_bin, x_min, NN, dd)
-
-	# print(f'g_dec = {g_dec}')
-
 	f_vals = np.array([rastrigin(g_dec[i]) for i in range(n)]).reshape((n, 1))
-
 	# if not (_ & 63):
-	print(f'it {_:0>4}: {np.max(f_vals):>12.7f}')
-
-	# print(f'f_vals = {f_vals}')
-
+	# 	print(f'it {_:0>4}: {np.max(f_vals):>12.7f}')
 	g_dec = np.hstack([g_dec, f_vals])
-
-	# print(f'g_dec[:, -1] = {g_dec[:, -1]}')
-
 	b, w = best(g_dec), worst(g_dec)
-
-	# print(
-	# 	f'b = {b}\n'
-	# 	f'w = {w}'
-	# )
-
 	g_best = np.asarray(g_dec[b, :-1]).flatten()
-
-	# print(f'g_best = {g_best}')
-
 	g_dec = np.delete(g_dec, w, axis=0)
-
-	# print(f'g_dec = {g_dec}, g_dec.shape = {g_dec.shape}')
-
 	num = adapt(np.asarray(g_dec[:, -1]).flatten())
-
-	# print(f'num = {num}, len(num) = {len(num)}')
-
 	g_dec = np.vstack([new_generation(g_dec[:, :-1], num), g_best])
 
-	# print(f'g_dec = {g_dec}')
-
 print(g_dec)
- 
